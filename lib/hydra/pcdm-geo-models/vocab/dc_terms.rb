82d3b1ebf927a13d319ad0184a30d0e85e012db6
Eric James <eric.james@yale.edu>
2015-06-24 11:58:13 -0400

    some initial vocab and model structure

---
+++ lib/hydra/pcdm-geo-models/vocab/dc_terms.rb
@@ -0,0 +1,42 @@
+require 'rdf'
+module GeoVocabularies
+  class DublinCoreTerms < RDF::StrictVocabulary("http://purl.org/dc/terms/")
+    # Property definitions
+    property :provenance,
+      comment:
+      domain:
+      range:
+      label:
+      type: "rdf:Property".freeze
+    property :references,
+      comment:
+      domain:
+      range:
+      label:
+      type: "rdf:Property".freeze
+    property :spacial,
+      comment:
+      domain:
+      range:
+      label:
+      type: "rdf:Property".freeze
+    property :temporal,
+      comment:
+      domain:
+      range:
+      label:
+      type: "rdf:Property".freeze
+    property :issued,
+      comment:
+      domain:
+      range:
+      label:
+      type: "rdf:Property".freeze
+    property :isPartOf,
+      comment:
+      domain:
+      range:
+      label:
+      type: "rdf:Property".freeze
+  end
+end      
