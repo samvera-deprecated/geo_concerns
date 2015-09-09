b88d1dc387237b185fc2630e0ed53f7a707f265b
Darren Hardy <drh@stanford.edu>
2015-09-09 15:59:19 -0700

    add RDF prefixes

--- geo_concerns/app/models/concerns/georeferenced_behavior.rb
+++ geo_concerns/app/models/concerns/georeferenced_behavior.rb
@@ -8,7 +8,7 @@ module GeoreferencedBehavior
     #   urn:ogc:def:crs,crs:EPSG::4269,crs:EPSG::5713
     #   http://www.opengis.net/def/crs/epsg/0/3163 (maybe? referred to in WFS query document)
     # See http://portal.opengeospatial.org/files/?artifact_id=30575
-    property :crs, predicate: ::RDF::URI.new("http://www.opengis.net/def/dataType/OGC/1.1/crsURI"), multiple: false do |index|
+    property :crs, predicate: ::RDF::URI.new(PREFIXES[:ogc] + 'crsURI'), multiple: false do |index|
       index.as :stored_searchable
     end
     
