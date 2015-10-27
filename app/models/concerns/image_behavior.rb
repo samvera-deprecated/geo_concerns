b5fbd3c4b46e4f839303caddf5ab01f82b8df5da
Darren Hardy <drh@stanford.edu>
2015-10-27 14:11:26 -0700

    use filter_associations for ImageBehavior and remove _id query code

--- app/models/concerns/image_behavior.rb
+++ app/models/concerns/image_behavior.rb
@@ -9,7 +9,11 @@ module ImageBehavior
     type [Hydra::PCDM::Vocab::PCDMTerms.Object,
       Hydra::Works::Vocab::WorksTerms.GenericWork,
       "http://projecthydra.org/geoconcerns/models#Image"]
+      filters_association :members, as: :image_files, condition: :image_file?
+      filters_association :members, as: :metadata_files, condition: :external_metadata_file?
+      filters_association :members, as: :rasters, condition: :raster?
   end
+
 
  # Defines type by what it is and isn't
   # @return [Boolean]
@@ -35,23 +39,23 @@ module ImageBehavior
     false
   end
 
-  #get types of members
-  def image_file
-    members.select(&:image_file?)
-  end
-  def image_file_id
-    image_file.map(&:id)
-  end
-  def metadata_files
-    members.select(&:external_metadata_file?)
-  end
-  def metadata_files_ids
-    metadata_files.map(&:id)
-  end
-  def rasters
-    members.select(&:raster?)
-  end
-  def rasters_ids
-    rasters.map(&:id)
-  end
+  # #get types of members
+  # def image_file
+  #   members.select(&:image_file?)
+  # end
+  # def image_file_id
+  #   image_file.map(&:id)
+  # end
+  # def metadata_files
+  #   members.select(&:external_metadata_file?)
+  # end
+  # def metadata_files_ids
+  #   metadata_files.map(&:id)
+  # end
+  # def rasters
+  #   members.select(&:raster?)
+  # end
+  # def rasters_ids
+  #   rasters.map(&:id)
+  # end
 end
