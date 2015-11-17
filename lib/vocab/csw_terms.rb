b1ccb34ad2be3bdc9249c3ace721e25e6a577929
griffinj@lafayette.edu <griffinj@lafayette.edu>
2015-11-17 18:19:05 +0000

    Removing the unused Vocab::CswTerms Class (it was determined as preferable to implement the desired attribute using another ontology)

--- lib/vocab/csw_terms.rb
+++
@@ -1,7 +0,0 @@
-require 'rdf'
-module Vocab
-  class CswTerms < RDF::Vocabulary('http://www.opengis.net/def/serviceType/ogc/csw/2.0.2#')
-    # No terms have been used within any Concerns
-    # Further, this may best be restructured for the rdf-vocab Gem (https://github.com/ruby-rdf/rdf-vocab)
-  end
-end
