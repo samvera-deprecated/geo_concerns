ecdc85abef58df5967d2e1147e1c14a90e889dec
Eliot Jordan <eliotj@princeton.edu>
2015-11-25 16:26:56 -0500

    remove file actors

--- app/actors/curation_concerns/vector_file_actor.rb
+++
@@ -1,7 +0,0 @@
-# Generated via
-#  `rails generate curation_concerns:work VectorFile`
-module CurationConcerns
-  class VectorFileActor < CurationConcerns::BaseActor
-    include ::CurationConcerns::WorkActorBehavior
-  end
-end
