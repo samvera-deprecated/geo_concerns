d9fee4812207c3e5815302162e8921a5470b9b8a
Eliot Jordan <eliotj@princeton.edu>
2015-11-30 16:33:20 -0500

    add related vec and metadata files to vector work

--- app/presenters/vector_work_show_presenter.rb
+++ app/presenters/vector_work_show_presenter.rb
@@ -1,5 +1,10 @@
 class VectorWorkShowPresenter < CurationConcerns::WorkShowPresenter
   def vector_file_presenters
+
+    # get all file presenters
+    file_presenters
+
+    # filter for external vector files
     @file_sets.select do |file_set|
       FileSet.vector_file_formats.include? file_set
         .solr_document[:geo_file_format_tesim].first
@@ -7,13 +12,18 @@ class VectorWorkShowPresenter < CurationConcerns::WorkShowPresenter
   end
 
   def external_metadata_file_formats_presenters
+    
+    # get all file presenters
+    file_presenters
+
+    # filter for external metadata files
     @file_sets.select do |file_set|
       FileSet.external_metadata_file_formats.include? file_set
         .solr_document[:geo_file_format_tesim].first
     end
   end
 
-  # def file_presenter_class
-  #   ::FileSetPresenter
-  # end
+  def file_presenter_class
+    FileSetPresenter
+  end
 end
