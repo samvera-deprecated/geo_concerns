403bc36ab0e8fe9079a3458c88c839c26a0e8dd0
Eliot Jordan <eliotj@princeton.edu>
2016-08-30 20:16:59 -0500

    enhance bounding box selector

--- app/assets/javascripts/geo_concerns/bounding_box_selector.js
+++ app/assets/javascripts/geo_concerns/bounding_box_selector.js
@@ -1,4 +1,4 @@
-  function boundingBoxSelector(options) {
+function boundingBoxSelector(options) {
   var inputId = options.inputId;
   var initialBounds;
   var coverage = options.coverage;
@@ -18,6 +18,8 @@
     scrollWheelZoom: false
   }).fitBounds(initialBounds);
 
+  updateBboxInputs(initialBounds);
+
   L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     maxZoom: 18
   }).addTo(map);
@@ -28,12 +30,10 @@
   } else {
     boundingBox = new L.BoundingBox({ bounds: initialBounds,
                                    buttonPosition: 'topright', }).addTo(map);
-  boundingBox.on('change', function() {
-    $(inputId).val(boundsToCoverage(this.getBounds()));
-  });
 
     boundingBox.on('change', function() {
       $(inputId).val(boundsToCoverage(this.getBounds()));
+      updateBboxInputs(this.getBounds())
     });
 
     boundingBox.enable();
@@ -97,4 +97,12 @@ function boundsToCoverage(bounds) {
   catch (err) {
     return '';
   }
+};
+
+function updateBboxInputs(bounds) {
+  bounds = clampBounds(bounds);
+  $('#bbox-north').val(bounds.getNorth().toFixed(6));
+  $('#bbox-east').val(bounds.getEast().toFixed(6));
+  $('#bbox-south').val(bounds.getSouth().toFixed(6));
+  $('#bbox-west').val(bounds.getWest().toFixed(6));
 };
--- app/renderers/geo_concerns/coverage_renderer.rb
+++ app/renderers/geo_concerns/coverage_renderer.rb
@@ -1,5 +1,7 @@
 module GeoConcerns
   class CoverageRenderer < CurationConcerns::Renderers::AttributeRenderer
+    include BoundingBoxHelper
+
     def render
       coverage = values.first if values
       return '' unless coverage
@@ -11,26 +13,27 @@ module GeoConcerns
       def markup(coverage)
         markup = ''
         markup << %(<tr><th>#{label}</th>\n<td id='accordion'><ul class='tabular'>)
-        markup << value(coverage)
+        markup << %(<div id='bbox' class='collapse in'></div>)
+        markup << bbox_display_inputs
+        markup << bbox_script_tag(coverage)
         markup << toggle_button
-        markup << map(coverage)
         markup << %(</ul></td></tr>)
         markup
       end
 
-      def value(coverage)
-        attributes = microdata_object_attributes(field).merge(class: "attribute #{field}")
-        %(<li#{html_attributes(attributes)}>#{attribute_value_to_html(coverage.to_s)})
-      end
-
       def toggle_button
-        %( <a data-toggle='collapse' data-parent='accordion' href='#bbox' class='btn btn-default'>
-           Toggle Map</a>)
+        %(
+          <a data-toggle='collapse' data-parent='accordion' href='#bbox' class='btn btn-default'>
+           Toggle Map</a>
+          )
       end
 
-      def map(coverage)
-        %(<div id='bbox' class='collapse in'></div>
-          <script>boundingBoxSelector({coverage: '#{coverage}', readonly: true});</script>)
+      def bbox_script_tag(coverage)
+        %(
+          <script>
+            boundingBoxSelector({coverage: '#{coverage}', readonly: true});
+          </script>
+        )
       end
   end
 end
