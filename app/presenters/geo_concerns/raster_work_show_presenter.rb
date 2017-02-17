5f2311199178b7ad373e785dfb76602378f5379b
Eliot Jordan <eliot.jordan@gmail.com>
2017-02-17 16:45:08 -0600

    Remove unneeded Geoblacklight config (#303)

--- app/presenters/geo_concerns/raster_work_show_presenter.rb
+++ app/presenters/geo_concerns/raster_work_show_presenter.rb
@@ -1,6 +1,5 @@
 module GeoConcerns
   class RasterWorkShowPresenter < GeoConcernsShowPresenter
-    self.work_presenter_class = ::GeoConcerns::VectorWorkShowPresenter
     self.file_format_service = GeoConcerns::RasterFormatService
   end
 end
